75b865d53f02bbe91b5166d643e83c8eb3389a58
Hiram Ramirez <hialf.92@gmail.com>
2021-07-20 04:23:01 -0500

    Cambios de rama para petController

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgdi.services.PetService;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+    
+    public PetController(PetService ps) {
+    	this.petService = ps;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
